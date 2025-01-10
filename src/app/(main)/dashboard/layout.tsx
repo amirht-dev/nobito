9b02960e65d30e572bee221e8614bc74db14fbad
amirhossein taheri <amirht.dev@gmail.com>
2025-01-10 12:10:51 +0330

    add items-start to dashboard layout

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -6,7 +6,7 @@ const DashboardLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <DashboardBreadcrumb />
-      <div className="container mb-10 mt-6 flex gap-6 lg:mt-10">
+      <div className="container mb-10 mt-6 flex items-start gap-6 lg:mt-10">
         <DashboardSidebar className="max-lg:hidden" />
         <div className="flex-1">{children}</div>
       </div>
