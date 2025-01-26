6e929b627f047669cda3626c794fd981f419e844
amirhossein taheri <amirht.dev@gmail.com>
2025-01-26 23:04:52 +0330

    add forwardRef to Rating

--- src/components/atoms/Rating/index.tsx
+++ src/components/atoms/Rating/index.tsx
@@ -1,46 +1,62 @@
+"use client";
+
+import { cn } from "@/utils/helpers";
 import clsx from "clsx";
+import { forwardRef } from "react";
 import {
   Star1_Bold,
   Star1_Outline,
 } from "../icons/Support-Like-Question/Star1";
 import type { RatingProps } from "./index.types";
 
-const Rating = ({
-  count = 5,
-  value,
-  onChange,
-  disabled = false,
-  readonly = false,
-}: RatingProps) => {
-  return (
-    <div className="flex items-center gap-2">
-      {Array.from({ length: count }, (_, idx) => {
-        const active = value && idx <= value;
-        const Icon = active ? Star1_Bold : Star1_Outline;
+const Rating = forwardRef<HTMLDivElement, RatingProps>(
+  (
+    {
+      count = 5,
+      value,
+      onChange,
+      disabled = false,
+      readonly = false,
+      className,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <div
+        {...props}
+        ref={ref}
+        className={cn("flex items-center gap-2", className)}
+      >
+        {Array.from({ length: count }, (_, idx) => {
+          const active = value && idx <= value;
+          const Icon = active ? Star1_Bold : Star1_Outline;
 
-        const handleChange = (idx: number) => {
-          if (readonly || disabled) return;
-          onChange?.(idx);
-        };
+          const handleChange = (idx: number) => {
+            if (readonly || disabled) return;
+            onChange?.(idx);
+          };
 
-        return (
-          <button
-            key={idx}
-            onClick={() => handleChange(idx)}
-            aria-readonly={readonly}
-            aria-disabled={disabled}
-            disabled={disabled}
-            className={clsx(
-              "disabled:cursor-not-allowed disabled:opacity-40 aria-readonly:cursor-auto",
-              active ? "text-secondary-600" : "text-grey-500",
-            )}
-          >
-            <Icon className="size-6" />
-          </button>
-        );
-      })}
-    </div>
-  );
-};
+          return (
+            <button
+              key={idx}
+              onClick={() => handleChange(idx)}
+              aria-readonly={readonly}
+              aria-disabled={disabled}
+              disabled={disabled}
+              className={clsx(
+                "disabled:cursor-not-allowed disabled:opacity-40 aria-readonly:cursor-auto",
+                active ? "text-secondary-600" : "text-grey-500",
+              )}
+            >
+              <Icon className="size-6" />
+            </button>
+          );
+        })}
+      </div>
+    );
+  },
+);
+Rating.displayName = "Rating";
 
 export default Rating;
--- src/components/atoms/Rating/index.types.ts
+++ src/components/atoms/Rating/index.types.ts
@@ -1,9 +1,13 @@
-import type { ReactNode } from "react";
+import type { ComponentPropsWithoutRefWithProps } from "@/types/utils";
+import type { ComponentPropsWithoutRef, ReactNode } from "react";
 
-export type RatingProps = {
-  count?: number;
-  value?: number;
-  onChange?: (value: number) => void;
-  readonly?: boolean;
-  disabled?: boolean;
-};
+export type RatingProps = ComponentPropsWithoutRefWithProps<
+  "div",
+  {
+    count?: number;
+    value?: number;
+    onChange?: (value: number) => void;
+    readonly?: boolean;
+    disabled?: boolean;
+  }
+>;
